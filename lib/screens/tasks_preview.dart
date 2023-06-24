ac13dacfc0969dd61b19b8ccb9ddf84cc75de7b0
Castorsorcier <castorsorcier@gmail.com>
2023-06-24 15:15:48 +0200

    ajout fonctionnalité permettant de compléter ou décompléter une tâche en appuyant sur l'îcone de la tâche

--- lib/screens/tasks_preview.dart
+++ lib/screens/tasks_preview.dart
@@ -1,8 +1,12 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_list/providers/tasks_provider.dart';
 import 'package:todo_list/screens/tasks_details.dart';
 
 import '../models/task.dart';
+import '../todo_list_app.dart';
 
 class TaskPreview extends StatelessWidget {
   final Task? task;
@@ -23,15 +27,23 @@ class TaskPreview extends StatelessWidget {
         ),
       ),
       subtitle: Text(task?.content ?? ''),
-      leading: task!.completed
+      leading: IconButton(
+        onPressed: ()async{
+          task!.completed=!task!.completed;
+          await TasksProvider().modifyTask(task!);
+          Navigator.pushReplacement(context, MaterialPageRoute(
+              builder: (context) =>
+              const ToDoListApp()));
+        },
+        icon: task!.completed
           ? Icon(
-              Icons.check,
-              color: Colors.green,
-            )
+        Icons.check,
+        color: Colors.green,
+      )
           : Icon(
-              Icons.warning,
-              color: Colors.red,
-            ),
+        Icons.warning,
+        color: Colors.red,
+      ),),
       hoverColor: task!.completed
           ? const Color.fromRGBO(105, 240, 174, 1.0)
           : const Color.fromRGBO(255, 82, 82, 1.0),
